efd63cd0c20709f3c4f89f03030fb3f59a3c7375
riht7405 <fly200fly@gmail.com>
2026-02-27 00:37:10 +0500

    Низамов Айдар, ПрИ-201, Модуль 11.1

--- src/Lucky.java
+++ src/Lucky.java
@@ -4,33 +4,23 @@ public class Lucky {
     static AtomicInteger x = new AtomicInteger(0);
     static AtomicInteger count = new AtomicInteger(0);
 
-    static int x = 0;
-    static int count = 0;
-
-    synchronized static int getNextNumber() {
-        return x <= 999999 ? x++ : -1;
+    static int getNextNumber() {
+        int current = x.getAndIncrement();
+        return current <= 999999 ? current : -1;
     }
 
     static class LuckyThread extends Thread {
         @Override
         public void run() {
             while (true) {
-//                int currentX = x.incrementAndGet();
                 int currentX = getNextNumber();
-
                 if (currentX == -1) break;
-                if (currentX > 999999) {
-                    break;
-                }
 
-                if ((currentX % 10) + (currentX / 10) % 10 + (currentX / 100) % 10 ==
-                        (currentX / 1000) % 10 + (currentX / 10000) % 10 + (currentX / 100000) % 10) {
-
+                int sumFirst = currentX % 10 + (currentX / 10) % 10 + (currentX / 100) % 10;
+                int sumLast = (currentX / 1000) % 10 + (currentX / 10000) % 10 + (currentX / 100000) % 10;
+                if (sumFirst == sumLast) {
                     System.out.println(currentX);
-//                    count.incrementAndGet();
-                    synchronized (Lucky.class) {
-                        count++;
-                    }
+                    count.incrementAndGet();
                 }
             }
         }
@@ -49,7 +39,6 @@ public class Lucky {
         t2.join();
         t3.join();
 
-//        System.out.println("Total: " + count.get());
-        System.out.println("Total: " + count);
+        System.out.println("Total: " + count.get());
     }
 }
